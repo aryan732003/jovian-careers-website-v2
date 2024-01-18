7dba947e77c01405a0f0e756777ce7508a079491
aryan732003 <155628084+aryan732003@users.noreply.github.com>
2024-01-18 14:45:59 +0000

    load jobs from db

--- database.py
+++ database.py
@@ -12,8 +12,15 @@ engine = create_engine(db_connection_string,
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("select * from jobs"))
-
     jobs = []
-    for row in result._allrows():
-      jobs.append(row._mapping)
+    for row in result:
+      jobs_dict = (row._mapping)
+      jobs.append(dict(jobs_dict))
     return jobs
+
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM jobs WHERE id = :val"),
+                          {'val': id})
+    rows = result.all
